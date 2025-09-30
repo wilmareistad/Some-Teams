79a2becb2d51a6e26befb2f839faef97ab71b5e4
Wilma Reistad <wilma@MacBook-Pro-som-tillhor-Wilma.local>
2025-09-30 13:26:39 +0200

    Första commit lagt till H1 och footer

---
+++ data.php
@@ -0,0 +1,274 @@
+<?php
+
+$teams = [
+    'Barcelona' => [
+        'league' => 'Primera División (Spain)',
+        'uefa-coefficient-ranking' => 1,
+        'city' => 'Barcelona',
+        'url' => 'https://www.fcbarcelona.com/',
+        'logo' => 'https://upload.wikimedia.org/wikipedia/en/4/47/FC_Barcelona_%28crest%29.svg',
+        'opponents' => [
+            'Bayern Münich',
+            'Chelsea',
+            'Benfica',
+            'Roma',
+            'OH Leuven',
+            'Paris FC'
+        ]
+    ],
+    'Chelsea' => [
+        'league' => 'Women\’s Super League (England)',
+        'uefa-coefficient-ranking' => 3,
+        'city' => 'London',
+        'url' => 'https://www.chelseafc.com/',
+        'logo' => 'https://upload.wikimedia.org/wikipedia/en/c/cc/Chelsea_FC.svg',
+        'opponents' => [
+            'Barcelona',
+            'Wolfsburg',
+            'Roma',
+            'SKN St. Pölten',
+            'Paris FC',
+            'Twente'
+        ]
+    ],
+    'Wolfsburg' => [
+        'league' => 'Frauen-Bundesliga (Germany)',
+        'uefa-coefficient-ranking' => 5,
+        'city' => 'Wolfsburg',
+        'url' => 'https://www.vfl-wolfsburg.de/',
+        'logo' => 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/ce/VfL_Wolfsburg_Logo.svg/840px-VfL_Wolfsburg_Logo.svg.png?20150906163520',
+        'opponents' => [
+            'Chelsea',
+            'OL Lyonnes',
+            'Paris Saint-Germain',
+            'Real Madrid',
+            'Manchester United',
+            'Vålerenga'
+        ]
+    ],
+    'Roma' => [
+        'league' => 'Serie A (Italy)',
+        'uefa-coefficient-ranking' => 10,
+        'city' => 'Rome',
+        'url' => 'https://www.asroma.com/',
+        'logo' => 'https://upload.wikimedia.org/wikipedia/en/f/f7/AS_Roma_logo_%282017%29.svg',
+        'opponents' => [
+            'Barcelona',
+            'Chelsea',
+            'SKN St. Pölten',
+            'Real Madrid',
+            'Vålerenga',
+            'OH Leuven'
+        ]
+    ],
+    'Real Madrid' => [
+        'league' => 'Primera División (Spain)',
+        'uefa-coefficient-ranking' => 8,
+        'city' => 'Madrid',
+        'url' => 'https://www.realmadrid.com/',
+        'logo' => 'https://upload.wikimedia.org/wikipedia/en/5/56/Real_Madrid_CF.svg',
+        'opponents' => [
+            'Wolfsburg',
+            'Arsenal',
+            'Roma',
+            'Paris Saint-Germain',
+            'Paris FC',
+            'Twente'
+        ]
+    ],
+    'Bayern Munich' => [
+        'league' => 'Frauen-Bundesliga (Germany)',
+        'uefa-coefficient-ranking' => 4,
+        'city' => 'Munich',
+        'url' => 'https://fcbayern.com/',
+        'logo' => 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/1b/FC_Bayern_M%C3%BCnchen_logo_%282017%29.svg/1200px-FC_Bayern_M%C3%BCnchen_logo_%282017%29.svg.png',
+        'opponents' => [
+            'Arsenal',
+            'Barcelona',
+            'Juventus',
+            'Paris Saint-Germain',
+            'Vålerenga',
+            'Atletico de Madrid'
+        ]
+    ],
+    'Benfica' => [
+        'league' => 'campeonato nacional de futebol feminino (Portugal)',
+        'uefa-coefficient-ranking' => 11,
+        'city' => 'Lissabon',
+        'url' => 'https://www.slbenfica.pt/en-us/futebol/feminino',
+        'logo' => 'https://upload.wikimedia.org/wikipedia/en/thumb/a/a2/SL_Benfica_logo.svg/250px-SL_Benfica_logo.svg.png',
+        'opponents' => [
+            'Arsenal',
+            'Barcelona',
+            'Paris Saint-Germain',
+            'Juventus',
+            'Twente',
+            'Paris FC',
+        ]
+    ],
+    'Paris FC' => [
+        'league' => 'Division 1 Féminine (France)',
+        'uefa-coefficient-ranking' => 17,
+        'city' => 'Paris',
+        'url' => 'https://parisfc.fr/en/',
+        'logo' => 'https://upload.wikimedia.org/wikipedia/en/9/9f/Paris_FC_logo.svg',
+        'opponents' => [
+            'Barcelona',
+            'Chelsea',
+            'Benfica',
+            'Real Madrid',
+            'OH Leuven',
+            'Vålerenga',
+        ]
+    ],
+    'Paris Saint-Germain' => [
+        'league' => 'Division 1 Féminine (France)',
+        'uefa-coefficient-ranking' => 7,
+        'city' => 'Paris',
+        'url' => 'https://en.psg.fr/',
+        'logo' => 'https://upload.wikimedia.org/wikipedia/en/a/a7/Paris_Saint-Germain_F.C..svg',
+        'opponents' => [
+            'Bayern Münich',
+            'Wolfsburg',
+            'Real Madrid',
+            'Benfica',
+            'OH Leuven',
+            'Manchester United',
+        ]
+    ],
+    'Arsenal' => [
+        'league' => 'Women\’s Super League (England)',
+        'uefa-coefficient-ranking' => 6,
+        'city' => 'London',
+        'url' => 'https://www.arsenal.com/',
+        'logo' => 'https://upload.wikimedia.org/wikipedia/en/5/53/Arsenal_FC.svg',
+        'opponents' => [
+            'OL Lyonnes',
+            'Bayern Münich',
+            'Real Madrid',
+            'Benfica',
+            'Twente',
+            'OH Leuven',
+        ]
+    ],
+    'OL Lyonnes' => [
+        'league' => 'Division 1 Féminine (France)',
+        'uefa-coefficient-ranking' => 2,
+        'city' => 'Lyon',
+        'url' => 'https://www.ol.fr/en/football/women-pro/presentation-ol-lyonnes',
+        'logo' => 'https://upload.wikimedia.org/wikipedia/en/6/62/OL_Lyonnes.svg',
+        'opponents' => [
+            'Wolfsburg',
+            'Arsenal',
+            'SKN St. Pölten',
+            'Juventus',
+            'Atletico de Madrid',
+            'Manchester United',
+        ]
+    ],
+    'OH Leuven' => [
+        'league' => 'Lotto Super League (Belgium)',
+        'uefa-coefficient-ranking' => 57,
+        'city' => 'Heverlee',
+        'url' => 'https://ohleuven.com/',
+        'logo' => 'https://upload.wikimedia.org/wikipedia/commons/2/24/OH_LEUVEN.png',
+        'opponents' => [
+            'Arsenal',
+            'Barcelona',
+            'Roma',
+            'Paris Saint-Germain',
+            'Twente',
+            'Paris FC',
+        ]
+    ],
+    'Juventus' => [
+        'league' => 'Serie A (Italy)',
+        'uefa-coefficient-ranking' => 9,
+        'city' => 'Turin',
+        'url' => 'https://www.juventus.com/',
+        'logo' => 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/da/Juventus_Logo.png/1200px-Juventus_Logo.png',
+        'opponents' => [
+            'OL Lyonnes',
+            'Bayern Münich',
+            'Benfica',
+            'SKN St. Pölten',
+            'Manchester United',
+            'Atletico de Madrid',
+        ]
+    ],
+    'Twente' => [
+        'league' => 'Eredivisie (Netherlands)',
+        'uefa-coefficient-ranking' => 14,
+        'city' => 'Enschede',
+        'url' => 'https://www.fctwente.nl/',
+        'logo' => 'https://upload.wikimedia.org/wikipedia/en/thumb/e/e3/FC_Twente.svg/1200px-FC_Twente.svg.png',
+        'opponents' => [
+            'Chelsea',
+            'Arsenal',
+            'Real Madrid',
+            'Benfica',
+            'Atletico de Madrid',
+            'OH Leuven',
+        ]
+    ],
+    'Atletico de Madrid' => [
+        'league' => 'Primera División (Spain)',
+        'uefa-coefficient-ranking' => 28,
+        'city' => 'Madrid',
+        'url' => 'https://en.atleticodemadrid.com/noticias-primer-equipo-femenino',
+        'logo' => 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQ7WUXmeSddkoouLR88Lcp5iDpq71l3aDiCg&s',
+        'opponents' => [
+            'Bayern Münich',
+            'OL Lyonnes',
+            'Juventus',
+            'SKN St. Pölten',
+            'Manchester United',
+            'Twente',
+        ]
+    ],
+    'Vålerenga' => [
+        'league' => 'Toppserien (Norway)',
+        'uefa-coefficient-ranking' => 19,
+        'city' => 'Oslo',
+        'url' => 'https://www.vif-fotball.no/',
+        'logo' => 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/7d/V%C3%A5lerenga_Oslo_logo.svg/1280px-V%C3%A5lerenga_Oslo_logo.svg.png',
+        'opponents' => [
+            'Wolfsburg',
+            'Bayern Münich',
+            'SKN St. Pölten',
+            'Roma',
+            'Paris FC',
+            'Manchester United',
+        ]
+    ],
+    'SKN St. Pölten' => [
+        'league' => 'ÖFB-Frauenliga (Austria)',
+        'uefa-coefficient-ranking' => 13,
+        'city' => 'St. Pölten',
+        'url' => 'https://www.skn-frauen.at/',
+        'logo' => 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSkiSy4BkhZp1RgOL8cKly0rIU2uSXshWGSzQ&s',
+        'opponents' => [
+            'Chelsea',
+            'OL Lyonnes',
+            'Juventus',
+            'Roma',
+            'Atletico de Madrid',
+            'Vålerenga',
+        ]
+    ],
+    'Manchester United' => [
+        'league' => 'Women\’s Super League (England)',
+        'uefa-coefficient-ranking' => 27,
+        'city' => 'Manchester',
+        'url' => 'https://www.manutd.com/en/news/women',
+        'logo' => 'https://upload.wikimedia.org/wikipedia/en/7/7a/Manchester_United_FC_crest.svg',
+        'opponents' => [
+            'OL Lyonnes',
+            'Wolfsburg',
+            'Paris Saint-Germain',
+            'Juventus',
+            'Vålerenga',
+            'Atletico de Madrid',
+        ]
+    ],
+];
