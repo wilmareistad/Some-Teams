219734de835d4bd6024c317a3a80fb6c1a239233
Wilma <wilmareistad@icloud.com>
2025-10-09 11:43:11 +0200

    Lagt in min logga och mer info på about

--- about.php
+++ about.php
@@ -3,17 +3,35 @@
 <!DOCTYPE html>
 
 <body>
-    <nav class="links">
-        <a href="index.php">Start</a>
-        <a href="about.php">About</a>
+    <nav class="about-links">
+        <div class="container-logga">
+            <img class="logga" src="logga.png" alt="Logga">
+            <div class="nav-links">
+                <a href="index.php">Start</a>
+                <a href="about.php">About</a>
+            </div>
+        </div>
     </nav>
     <h1 class="about-h1">Hi! Im Wilma</h1>
-    <div class="container.about-h2">
+    <div class="container-about-h2">
         <h2 class="about-h2">Wilma - 22år - Kungälv</h2>
     </div>
-    <p>Hi! Im Wilma</p>
+    <h2 class="about-h2">Background</h2>
+    <p class="hi">Current Webutveckling @ YRGO</p>
+    <p class="hi">Graduated gymnasieingenjör @ Teknikcollage Nösnäsgymnasiet</p>
     <div class="container-me">
         <img class="picture-me" src="IMG_0429.jpeg" alt="Jag">
+        <div>
+            <h3>Mina intressen</h3>
+            <ul>
+                <li>Löpning</li>
+                <li>Klättring</li>
+                <li>Mat</li>
+                <li>Resa</li>
+                <li>Testa nya saker</li>
+            </ul>
+            <p>Mitt namn är Wilma och jag är ursprungligen från en liten håla vid namn Jörlanda som ligger i Stenungsundskommun. Jag är uppväxt på hästgård och har tävlat och tränat ridning största delen av livet. Jag har dock slutat nu och nu är mina största intressen löpning, klättring samt mat.</p>
+        </div>
     </div>
 
     <!-- <p><a href="index.php">Tillbaka till startsidan</a></p> -->
